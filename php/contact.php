44165764c35ad02e161ed33e5f98877cb34d2c0d
Matheus Link <matheuslink1996@gmail.com>
2016-06-12 18:47:03 -0300

    Formulário de contato atualizado.

--- php/contact.php
+++ php/contact.php
@@ -7,25 +7,48 @@ $para = "matheuslink1996@gmail.com";
 
 $nome = $_POST['name'];
 $email = $_POST['email'];
-$message = $_POST['comments'];
+$telefone = $_POST['telefone'];
 $subject = $_POST['subject'];
+$message = $_POST['comments'];
 
 //Mensagem
 $mensagem = "<b>Nome:</b> ".$nome;
 $mensagem .= "<br><b>Email:</b> ".$email;
+$mensagem .= "<br><b>Telefone:</b> ".$telefone;
 $mensagem .= "<br><b>Mensagem:</b> "."[".$subject."] ".$message;
 
 $headers = "Content-Type:text/html; charset=UTF-8\n";
-$headers .= "From: $email<$email>\n"; //Vai ser //mostrado que o email partiu deste email e seguido do nome
-//$headers .= "X-Sender: <haniger@haniger.com.br>\n"; //email do servidor //que enviou
+$headers .= "From: $email<$email>\n";
 $headers .= "X-Mailer: PHP v".phpversion()."\n";
 $headers .= "X-IP: ".$_SERVER['REMOTE_ADDR']."\n";
-//$headers .= "Return-Path: <guilherme@haniger.com.br>\n"; //caso a msg //seja respondida vai para este email.
 $headers .= "MIME-Version: 1.0\n";
 
 mail($para, "[".$subject."] Contato via Site", $mensagem, $headers);
 
-echo "<script>alert(\"Mensagem enviada com Sucesso!\");</script>";
-echo "<script>window.location.assign(\"contato.html\");</script>";
+/* teste */
+
+
+//campos
+$nome2 = $_POST['name'];
+$email2 = $_POST['email'];
+
+$from2 = "matheuslink1996@gmail.com";
+
+$para2 = $email2;
+//email de envio email
+
+//Mensagem
+$mensagem2 = "Olá, ".$nome.". Agradecemos o seu contato e estaremos retornando o mais rápído possível!";
+
+$headers2 = "Content-Type:text/html; charset=UTF-8\n";
+$headers2 .= "From: $from2<$from2>\n"; //Vai ser //mostrado que o email partiu deste email e seguido do nome
+$headers2 .= "X-Mailer: PHP v".phpversion()."\n";
+$headers2 .= "X-IP: ".$_SERVER['REMOTE_ADDR']."\n";
+$headers2 .= "MIME-Version: 1.0\n";
+
+mail($para2, "Mensagem Recebida!", $mensagem2, $headers2);
+
+echo "<script>alert('Mensagem enviada!');</script>";
+echo "<script>window.location.assign('../index.html');</script>";
 
 ?>
