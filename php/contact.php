9ddbe52a9f7e815b56f0e2ac2288913e5ab91f14
Matheus Link <matheuslink1996@gmail.com>
2016-06-12 00:32:02 -0300

    adicionado campo de assunto no formulário de email

--- php/contact.php
+++ php/contact.php
@@ -8,11 +8,12 @@ $para = "matheuslink1996@gmail.com";
 $nome = $_POST['name'];
 $email = $_POST['email'];
 $message = $_POST['comments'];
+$subject = $_POST['subject'];
 
 //Mensagem
 $mensagem = "<b>Nome:</b> ".$nome;
 $mensagem .= "<br><b>Email:</b> ".$email;
-$mensagem .= "<br><b>Mensagem:</b> ".$message;
+$mensagem .= "<br><b>Mensagem:</b> "."[".$subject."] ".$message;
 
 $headers = "Content-Type:text/html; charset=UTF-8\n";
 $headers .= "From: $email<$email>\n"; //Vai ser //mostrado que o email partiu deste email e seguido do nome
@@ -22,7 +23,7 @@ $headers .= "X-IP: ".$_SERVER['REMOTE_ADDR']."\n";
 //$headers .= "Return-Path: <guilherme@haniger.com.br>\n"; //caso a msg //seja respondida vai para este email.
 $headers .= "MIME-Version: 1.0\n";
 
-mail($para, "Contato Via Site", $mensagem, $headers);
+mail($para, "[".$subject."] Contato via Site", $mensagem, $headers);
 
 echo "<script>alert(\"Mensagem enviada com Sucesso!\");</script>";
 echo "<script>window.location.assign(\"contato.html\");</script>";
